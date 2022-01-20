f900d0cfb071fad936e94248ce728a94b000d5e6
Guillaume Quimper <guillaume.quimper@blacheyong.com>
2022-01-20 18:13:04 -0500

    Fix build transforms (px + shadow) Remove variable interpolations

--- build.js
+++ build.js
@@ -5,50 +5,63 @@ const StyleDictionaryPackage = require('style-dictionary');
 StyleDictionaryPackage.registerFormat({
     name: 'css/variables',
     formatter: function (dictionary, config) {
-      return `${this.selector} {
+        return `${this.selector} {
         ${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}
       }`
     }
-  });  
+});
 
 StyleDictionaryPackage.registerTransform({
     name: 'sizes/px',
     type: 'value',
-    matcher: function(prop) {
+    matcher: function (prop) {
         // You can be more specific here if you only want 'em' units for font sizes    
-        return ["fontSizes", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
+        return ["fontSizes", "lineHeights", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.type);
     },
-    transformer: function(prop) {
+    transformer: function (prop) {
         // You can also modify the value here if you want to convert pixels to ems
         return parseFloat(prop.original.value) + 'px';
     }
-    });
+});
+
+StyleDictionaryPackage.registerTransform({
+    name: 'shadow/spreadShadow',
+    type: 'value',
+    matcher: function (prop) {
+        return prop.type === 'boxShadow';
+    },
+    transformer: function (prop) {
+        const shadow = Object.values(prop.value);
+        const [x, y, blur, spread, color] = shadow.map((s) => s.toString());
+        return `${x}px ${y}px ${blur}px ${spread}px ${color}`;
+    } 
+});
 
 function getStyleDictionaryConfig(theme) {
-  return {
-    "source": [
-      `tokens/${theme}.json`,
-    ],
-    "platforms": {
-      /* "web": {
-        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
-        "buildPath": `output/`,
-        "files": [{
-            "destination": `${theme}.css`,
-            "format": "css/variables",
-            "selector": `.${theme}-theme`
-          }]
-      }, */
-      "scss": {
-        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
-        "buildPath": `src/styles/settings/`,
-        "files": [{
-          "destination": `${theme}.scss`,
-          "format": "scss/variables"
-        }]
-      }
-    }
-  };
+    return {
+        "source": [
+            `tokens/${theme}.json`,
+        ],
+        "platforms": {
+            /* "web": {
+              "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
+              "buildPath": `output/`,
+              "files": [{
+                  "destination": `${theme}.css`,
+                  "format": "css/variables",
+                  "selector": `.${theme}-theme`
+                }]
+            }, */
+            "scss": {
+                "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px", "shadow/spreadShadow"],
+                "buildPath": `src/styles/settings/`,
+                "files": [{
+                    "destination": `${theme}.scss`,
+                    "format": "scss/variables"
+                }]
+            }
+        }
+    };
 }
 
 console.log('Build started...');
