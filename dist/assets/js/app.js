69056691a3e514c31496bb80ab28768cec821056
MathieuBYC <mathieu.boulianne@blacheyong.com>
2022-02-03 15:49:53 -0500

    Fix headings/body-text mixins for multiple breakpoints

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -108,12 +108,9 @@ console.log('Hello world!');
   !*** ./src/styles/main.scss ***!
   \******************************/
 /*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: argument `$map` of `map-get($map, $key)` must be a map\n        on line 78 of src/styles/mixins/_typography.scss, in function `map-get`\n        from line 78 of src/styles/mixins/_typography.scss, in mixin `body-text`\n        from line 42 of src/styles/base/_typography.scss\n        from line 43 of src/styles/main.scss\n>>     @if(map-get($regular, font-family) != $font-family-base) {\n\n   --------^\n\n    at /Users/mathieu/www-root/byc-design-token-example/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Users/mathieu/www-root/byc-design-token-example/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/mathieu/www-root/byc-design-token-example/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/mathieu/www-root/byc-design-token-example/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.callback (/Users/mathieu/www-root/byc-design-token-example/node_modules/sass-loader/dist/index.js:62:7)\n    at Object.done [as callback] (/Users/mathieu/www-root/byc-design-token-example/node_modules/neo-async/async.js:8069:18)\n    at options.error (/Users/mathieu/www-root/byc-design-token-example/node_modules/node-sass/lib/index.js:293:32)");
 
 /***/ }),
 
