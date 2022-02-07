8494085c19c04801451a0bd863f5857323aa2ebb
MathieuBYC <mathieu.boulianne@blacheyong.com>
2022-02-07 11:48:30 -0500

    New addons (grid + design tokens) and testing sb-tokens

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,9 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     '@storybook/addon-a11y',
-    "storybook-addon-pseudo-states"
+    "storybook-addon-pseudo-states",
+    'storybook-design-token',
+    require.resolve('storybook-addon-grid/preset'),
   ],
   "framework": "@storybook/html"
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,15 @@
 import '!style-loader!css-loader!resolve-url-loader!sass-loader!./scss-loader.scss';
 
+const tokenContext = require.context(
+  '!!raw-loader!../src',
+  true,
+  /.\.(css|less|scss|svg)$/
+);
+
+const tokenFiles = tokenContext.keys().map(function (filename) {
+  return { filename: filename, content: tokenContext(filename).default };
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,5 +17,15 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  designToken: {
+    files: tokenFiles
+  },
+  grid: {
+    gridOn: false,
+    columns: 12,
+    gap: '20px',
+    gutter: '50px',
+    maxWidth: '1024px',
   },
 }
