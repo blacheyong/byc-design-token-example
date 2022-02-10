a32f99201bbd6d46fbba1e30696aa652f8195a76
MathieuBYC <mathieu.boulianne@blacheyong.com>
2022-02-10 11:42:17 -0500

    Radio + Checkbox + Select stories, fix checked disabled style, fix box-shadow/outline on buttons

---
+++ src/stories/Checkbox.stories.js
@@ -0,0 +1,63 @@
+export default {
+    title: 'Form/Checkbox',
+}
+
+export const Default = ({  }) => `
+<div class="form-check">
+    <input class="custom-check" type="checkbox" name="examplecheckboxs" id="examplecheckboxs1" value="option1" checked>
+    <label class="form-check-label" for="examplecheckboxs1">
+        Default checkbox
+    </label>
+</div>
+<div class="form-check">
+    <input class="custom-check" type="checkbox" name="examplecheckboxs" id="examplecheckboxs2" value="option2">
+    <label class="form-check-label" for="examplecheckboxs2">
+        Second default checkbox
+    </label>
+</div>
+<div class="form-check">
+    <input class="custom-check" type="checkbox" name="examplecheckboxs" id="examplecheckboxs3" value="option3">
+    <label class="form-check-label" for="examplecheckboxs3">
+        Third default checkbox
+    </label>
+</div>`
+
+export const Large = ({  }) => `
+<div class="form-check">
+    <input class="custom-check custom-check--lg" type="checkbox" name="examplecheckboxs" id="examplecheckboxs1" value="option1" checked>
+    <label class="form-check-label" for="examplecheckboxs1">
+        Large checkbox
+    </label>
+</div>
+<div class="form-check">
+    <input class="custom-check custom-check--lg" type="checkbox" name="examplecheckboxs" id="examplecheckboxs2" value="option2">
+    <label class="form-check-label" for="examplecheckboxs2">
+        Second large checkbox
+    </label>
+</div>
+<div class="form-check">
+    <input class="custom-check custom-check--lg" type="checkbox" name="examplecheckboxs" id="examplecheckboxs3" value="option3">
+    <label class="form-check-label" for="examplecheckboxs3">
+        Third large checkbox
+    </label>
+</div>`
+
+export const Disabled = ({  }) => `
+<div class="form-check">
+    <input class="custom-check" type="checkbox" name="examplecheckboxs" id="examplecheckboxs1" value="option1" checked disabled>
+    <label class="form-check-label" for="examplecheckboxs1">
+        Disabled checkbox
+    </label>
+</div>
+<div class="form-check">
+    <input class="custom-check" type="checkbox" name="examplecheckboxs" id="examplecheckboxs2" value="option2" disabled>
+    <label class="form-check-label" for="examplecheckboxs2">
+        Second disabled checkbox
+    </label>
+</div>
+<div class="form-check">
+    <input class="custom-check" type="checkbox" name="examplecheckboxs" id="examplecheckboxs3" value="option3" disabled>
+    <label class="form-check-label" for="examplecheckboxs3">
+        Third disabled checkbox
+    </label>
+</div>`
